66eafdf2d38d55917fcfa1c9fe7f58cef86aef87
Andy Quinn <andy.kettler.quinn@gmail.com>
2024-07-12 22:12:02 -0500

    finish chapter 2

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,24 @@
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
+import { MantineProvider } from '@mantine/core'
+import theme from 'config/mantine.config'
+import '@mantine/core/styles.css'
+
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
+import './scaffold.css'
 import './index.css'
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <RedwoodApolloProvider>
-        <Routes />
-      </RedwoodApolloProvider>
+      <MantineProvider theme={theme}>
+        <RedwoodApolloProvider>
+          <Routes />
+        </RedwoodApolloProvider>
+      </MantineProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
